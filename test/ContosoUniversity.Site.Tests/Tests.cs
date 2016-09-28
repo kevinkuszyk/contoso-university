12728d1e261f261a02afa11555b9fc66abc9c836
Kevin Kuszyk <kevin@kevinkuszyk.com>
2016-09-28 21:26:13 +0100

    Add test project

---
+++ test/ContosoUniversity.Site.Tests/Tests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ContosoUniversity.Site.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Foo_should_be_true()
+        {
+            // Arrange
+            var foo = true;
+
+            // Assert
+            foo.Should().BeTrue();
+        }
+
+        [Test]
+        public void Bar_should_be_false()
+        {
+            // Arrange
+            var bar = false;
+
+            // Assert
+            bar.Should().BeFalse();
+        }
+    }
+}
