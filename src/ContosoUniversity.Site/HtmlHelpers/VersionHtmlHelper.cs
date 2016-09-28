1c52f99a1e3232f26d8c637fa3470db6280f96ed
Kevin Kuszyk <kevin@kevinkuszyk.com>
2016-09-28 21:45:41 +0100

    Show version number in page footer

---
+++ src/ContosoUniversity.Site/HtmlHelpers/VersionHtmlHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Site.HtmlHelpers
+{
+    public static class VersionHtmlHelper
+    {
+        public static IHtmlString GetVersion(this HtmlHelper html)
+        {
+            var assembly = typeof(MvcApplication).Assembly;
+
+            var attribute = assembly.GetCustomAttributes<AssemblyInformationalVersionAttribute>()
+                                    .FirstOrDefault();
+
+            var version = attribute?.InformationalVersion ?? assembly.GetName().Version.ToString();
+
+            return new MvcHtmlString(version);
+        }
+    }
+}
