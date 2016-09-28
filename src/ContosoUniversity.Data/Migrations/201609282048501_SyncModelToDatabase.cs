1e18c40587b336a29b190983804cb3f32a757c06
Kevin Kuszyk <kevin@kevinkuszyk.com>
2016-09-28 21:51:07 +0100

    Adding missing database migration

---
+++ src/ContosoUniversity.Data/Migrations/201609282048501_SyncModelToDatabase.cs
@@ -0,0 +1,18 @@
+namespace ContosoUniversity.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SyncModelToDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
